f044ff260383cbf77f500ee0569418a4a86ee2f7
Mike Perham <mperham@gmail.com>
2008-07-08 11:43:32 -0500

    Add gem manifest.  Fix integration tests.

--- example/test/integration/account_figments_test.rb
+++ example/test/integration/account_figments_test.rb
@@ -5,6 +5,8 @@ class AccountFigmentsTest < ActionController::IntegrationTest
   def test_create_account_and_figments
     conn0 = db_connection "shard_0_test"
     conn1 = db_connection "shard_1_test"
+    conn0.clear 'figments'
+    conn1.clear 'figments'
     assert_equal 0, conn0.count_for('figments')
     assert_equal 0, conn1.count_for('figments')
 
@@ -14,14 +16,29 @@ class AccountFigmentsTest < ActionController::IntegrationTest
       user.selects_account(mike)
       before = mike.figments.size
       user.creates_figment(14)
-      mike.figments.reload!
+      mike.figments.reload
       assert_equal before + 1, mike.figments.size
       assert_equal 14, mike.figments.first.value
     end
 
-    # Bypass data_fabric and verify the table contents.
+    # Bypass data_fabric and verify the figment went to shard 0.
     assert_equal 1, conn0.count_for('figments')
     assert_equal 0, conn1.count_for('figments')
+
+    new_session do |user|
+      user.goes_home
+      bob = user.creates_account('bob', '1')
+      user.selects_account(bob)
+      before = bob.figments.size
+      user.creates_figment(66)
+      bob.figments.reload
+      assert_equal before + 1, bob.figments.size
+      assert_equal 66, bob.figments.first.value
+    end
+
+    # Bypass data_fabric and verify the figment went to shard 1.
+    assert_equal 1, conn0.count_for('figments')
+    assert_equal 1, conn1.count_for('figments')
   end
 
   private
@@ -30,6 +47,9 @@ class AccountFigmentsTest < ActionController::IntegrationTest
     conn = ActiveRecord::Base.sqlite3_connection(HashWithIndifferentAccess.new(ActiveRecord::Base.configurations[conf]))
     def conn.count_for(table)
       Integer(execute("select count(*) as c from #{table}")[0]['c'])
+    end
+    def conn.clear(table)
+      execute("delete from #{table}")
     end
     conn
   end
@@ -70,44 +90,6 @@ class AccountFigmentsTest < ActionController::IntegrationTest
       follow_redirect!
       assert_response :success
       assert_template "accounts/index"
-    end
-
-    def uploads_photo(user)
-      assert_nil user.photo
-      multipart_post user_url(accounts(:fiveruns), user), {:hidden_user_id => user.id, :_method => :put, :user => {:photo_data => fixture_file_upload("/photos/sam.jpg", "image/jpeg")}}
-      assert_response :redirect
-      follow_redirect!
-      assert_response :success
-      assert_template "users/show"
-
-      user = User.find(user.id)
-      assert_not_nil user.photo
-      assert_not_nil user.photo.current_data
-      assert_select 'img[alt=Photo]', {:count => 1} do
-        assert_select "[src=?]", /^\/a\/fiveruns\/users\/[\d]+\/photo\?[\d]+$/
-      end
-    end
-
-    def removes_photo(user)
-      user = User.find(user.id)
-
-      assert_not_nil user.photo
-      post user_photo_url(accounts(:fiveruns), user), {:_method => :delete}
-      assert_response :redirect
-      follow_redirect!
-      assert_response :success
-      assert_template "users/edit"
-
-      user = User.find(user.id)
-      assert_nil user.photo
-
-      assert_photo_is_nil
-    end
-
-    def assert_photo_is_nil
-      assert_select 'img[alt=Photo]', {:count => 1} do
-        assert_select "[src=?]", /^\/a\/fiveruns\/users\/[\d]+\/photo\?[\d]+$/
-      end
     end
   end
 end
