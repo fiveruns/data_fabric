a11b79bc0d96284e82d95ed2e217b899978d08cd
Mike Perham <mperham@gmail.com>
2009-02-06 13:43:00 -0600

    Add Dash recipe for data_fabric

---
+++ lib/data_fabric/dash.rb
@@ -0,0 +1,20 @@
+# A data_fabric recipe for use with the FiveRuns Dash metrics service at 
+# http://dash.fiveruns.com.
+#
+# Hook into your Rails application by adding the recipe in your 
+# config/initializers/dash.rb, like so:
+#
+# require 'data_fabric/dash'
+# Fiveruns::Dash::Rails.start :production => 'your-token' do |config|
+#   config.add_recipe :data_fabric, 'http://mikeperham.com'
+# end
+#
+raise ArgumentError, "The Dash recipe for DataFabric is only supported on ActiveRecord 2.2 and greater" if ActiveRecord::VERSION::STRING < '2.2.0'
+
+Fiveruns::Dash.register_recipe :data_fabric, 'http://mikeperham.com' do |recipe|
+  recipe.absolute :open_connections, 'Open Connections' do
+    ConnectionProxy.shard_pools.map do |pool|
+      pool.instance_variable_get(:@checked_out) || 0
+    end.sum
+  end
+end
